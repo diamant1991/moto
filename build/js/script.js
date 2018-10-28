b323420c4030d9a25eb13121531840732a509d78
diamant1991 <diamant1988@mail.ru>
2018-10-28 20:29:45 +0200

    catalog, card pages

--- build/js/script.js
+++ build/js/script.js
@@ -10,3 +10,26 @@ $('.dropdown-toggle, .sidebar__catalog__title').click(function() {
 		drop.slideUp(350)
 	}
 });
+
+ $('.product__slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.product__thumb'
+});
+
+$('.product__thumb').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.product__slider',
+  dots: false,
+  margin: 10,
+  focusOnSelect: true
+});
+
+$('.viewed__slider').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  dots: false
+});
