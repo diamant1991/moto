178c30bd946eb548200244f758531141c677889b
diamant1991 <diamant1988@mail.ru>
2018-10-29 16:54:00 +0200

    card, basket

--- build/js/script.js
+++ build/js/script.js
@@ -25,11 +25,57 @@ $('.product__thumb').slick({
   asNavFor: '.product__slider',
   dots: false,
   margin: 10,
-  focusOnSelect: true
+  focusOnSelect: true,
+  responsive: [
+    {
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
 
 $('.viewed__slider').slick({
   slidesToShow: 5,
   slidesToScroll: 1,
-  dots: false
+  dots: false,
+  responsive: [
+    {
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
